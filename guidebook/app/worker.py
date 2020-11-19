4881947d771c0bb92afe47eec63d7edce47030f0
Forrest Collman <forrest.collman@gmail.com>
2020-11-18 16:35:01 -0800

    changing redis config

--- guidebook/app/worker.py
+++ guidebook/app/worker.py
@@ -3,7 +3,13 @@ import redis
 from rq import Worker, Queue, Connection
 
 listen = ['default']
-redis_url = os.getenv('GUIDEBOOK_REDIS_URL', 'redis://localhost:6379')
+REDIS_HOST = os.environ.get('REDIS_SERVICE_HOST','localhost')
+REDIS_PORT = os.environ.get('REDIS_SERVICE_PORT',6379)
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD','')
+if len(REDIS_PASSWORD)==0:
+    redis_url = f'redis://{REDIS_HOST}:{REDIS_PORT}/0'
+else:
+    redis_url = f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0'
 
 conn = redis.from_url(redis_url)
 
