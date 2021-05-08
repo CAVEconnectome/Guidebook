ab92a8b3629c32c6b40e5e4ee12b4c4b341081ed
Casey Schneider-Mizell <caseysm@gmail.com>
2021-05-07 22:36:42 -0700

    Bump version: 0.0.13 → 0.0.14

--- guidebook/blueprint.py
+++ guidebook/blueprint.py
@@ -14,7 +14,7 @@ api_prefix = f"/api/v{api_version}"
 
 
 bp = Blueprint("guidebook", __name__, url_prefix=url_prefix)
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 
 q = Queue(connection=conn)
 
