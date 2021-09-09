e18a67b3fdf640833baeecbb02fb1008f4e280a6
Casey Schneider-Mizell <caseysm@gmail.com>
2021-09-09 14:23:24 -0400

    Omit cover path operation

--- guidebook/blueprint.py
+++ guidebook/blueprint.py
@@ -102,7 +102,7 @@ def generate_guidebook_chunkgraph(datastack):
     return redirect(url_for(".show_result_points", job_key=job.get_id()))
 
 
-@bp.route("coverpaths/results/<job_key>")
+@bp.route("skeletonize/results/<job_key>")
 @auth_required
 def show_result_points(job_key):
     reload_time = 10
@@ -112,27 +112,6 @@ def show_result_points(job_key):
             return render_template(
                 "show_link.html",
                 state_name="Branch and End Point",
-                ngl_url=job.result,
-                version=__version__,
-            )
-        elif job.get_status() == "failed":
-            return error_page(job.exc_info)
-        else:
-            return wait_page(reload_time)
-    except Exception as e:
-        return error_page(str(e))
-
-
-@bp.route("skeletonize/results/<job_key>")
-@auth_required
-def show_result_paths(job_key):
-    reload_time = 60
-    try:
-        job = Job.fetch(job_key, connection=conn)
-        if job.is_finished:
-            return render_template(
-                "show_link.html",
-                state_name="Path Review",
                 ngl_url=job.result,
                 version=__version__,
             )
@@ -204,118 +183,6 @@ def lvl2_point_form():
 
     return render_template(
         "lvl2_points.html",
-        title="Neuron Guidebook",
-        form=form,
-        version=__version__,
-        allow_segmentation=current_app.config.get("ALLOW_SEGMENTATION", False),
-    )
-
-
-@bp.route(f"{api_prefix}/datastack/<datastack>/l2paths")
-@auth_required
-def generate_guidebook_paths(datastack):
-    root_id = request.args.get("root_id", None)
-    if root_id is not None:
-        root_id = int(root_id)
-    root_loc = parse_location(request.args.get("root_location", None))
-    collapse_soma = request.args.get("collapse_soma") == "True"
-
-    split_loc = parse_location(request.args.get("split_location", None))
-    downstream = request.args.get("downstream") == "True"
-    root_id_from_point = request.args.get("root_id_from_point") == "True"
-    spacing = request.args.get("spacing", 3000)
-
-    num_paths_raw = request.args.get("num_paths", "all")
-    num_path_dict = {"all": "all", "5": 5, "10": 10, "15": 15}
-    num_paths = num_path_dict.get(num_paths_raw, "all")
-
-    exclude_short = request.args.get("exclude_short", "True") == "True"
-    if exclude_short:
-        segment_length_thresh = SHORT_SEGMENT_THRESH
-    else:
-        segment_length_thresh = 0
-
-    root_point_resolution = current_app.config.get(
-        "GUIDEBOOK_EXPECTED_RESOLUTION", [4, 4, 40]
-    )
-    print(f"Resolution: {root_point_resolution}")
-
-    kwargs = {
-        "datastack": datastack,
-        "server_address": current_app.config.get("GLOBAL_SERVER_ADDRESS"),
-        "return_as": "url",
-        "root_id": root_id,
-        "root_point": root_loc,
-        "root_point_resolution": root_point_resolution,
-        "n_choice": num_paths,
-        "segment_length_thresh": segment_length_thresh,
-        "spacing": int(spacing),
-        "collapse_soma": collapse_soma,
-        "n_parallel": int(current_app.config.get("N_PARALLEL")),
-        "invalidation_d": int(current_app.config.get("INVALIDATION_D")),
-        "selection_point": split_loc,
-        "downstream": downstream,
-        "root_id_from_point": root_id_from_point,
-        "auth_token_key": current_app.config.get("AUTH_TOKEN_KEY"),
-    }
-    print(kwargs)
-    job = q.enqueue_call(
-        generate_lvl2_paths,
-        kwargs=kwargs,
-        result_ttl=5000,
-        timeout=600,
-        retry=Retry(max=2, interval=10),
-    )
-    return redirect(url_for(".show_result_paths", job_key=job.get_id()))
-
-
-@bp.route("coverpaths", methods=["GET", "POST"])
-@auth_required
-def lvl2_path_form():
-    form = Lvl2PathForm()
-    if form.validate_on_submit():
-        datastack = current_app.config.get("DATASTACK")
-        root_id = form.root_id.data
-        if len(root_id) == 0:
-            root_id = None
-        segmentation_fallback = form.segmentation_fallback.data
-
-        try:
-            root_loc_formatted = encode_root_location(form.root_location.data)
-        except Exception as e:
-            return error_page(e)
-        root_is_soma = form.root_is_soma.data
-
-        split_location_formatted = encode_root_location(form.split_location.data)
-        if form.split_option.data == "downstream":
-            downstream = True
-        else:
-            downstream = False
-        root_id_from_point = form.root_id_from_root_loc.data
-
-        num_paths_raw = form.num_paths.data
-        num_path_dict = {"all": "all", "five": 5, "ten": 10, "fifteen": 15}
-        num_paths = num_path_dict.get(num_paths_raw, "all")
-
-        exclude_short = form.exclude_short.data
-
-        url = url_for(
-            ".generate_guidebook_paths",
-            datastack=datastack,
-            root_id=root_id,
-            root_location=root_loc_formatted,
-            collapse_soma=root_is_soma,
-            num_paths=num_paths,
-            exclude_short=exclude_short,
-            segmentation_fallback=segmentation_fallback,
-            split_location=split_location_formatted,
-            downstream=downstream,
-            root_id_from_point=root_id_from_point,
-        )
-        return redirect(url)
-
-    return render_template(
-        "lvl2_paths.html",
         title="Neuron Guidebook",
         form=form,
         version=__version__,
