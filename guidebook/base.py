a304460b17aa68f80b4e416ff797aea07a703b2f
Casey Schneider-Mizell <caseysm@gmail.com>
2021-09-09 17:55:15 -0400

    remove cover_review code

--- guidebook/base.py
+++ guidebook/base.py
@@ -5,7 +5,6 @@ from caveclient import CAVEclient
 from nglui import statebuilder as sb
 from pcg_skel.chunk_tools import get_root_id_from_point, get_closest_lvl2_chunk
 from .topo_points import topo_point_construction
-from .cover_review import construct_cover_paths
 
 from .parameters import (
     GUIDEBOOK_EXPECTED_RESOLUTION,
@@ -36,88 +35,6 @@ def mask_skeleton(
         mask[selection_skid] = ~mask[selection_skid]
 
     return sk.apply_mask(mask), selection_l2id
-
-
-def generate_lvl2_paths(
-    datastack,
-    root_id,
-    server_address,
-    root_point=None,
-    root_point_resolution=GUIDEBOOK_EXPECTED_RESOLUTION,
-    n_choice="all",
-    segment_length_thresh=0,
-    spacing=PATH_SPACING,
-    interp_method="linear",
-    selection_point=None,
-    downstream=True,
-    invalidation_d=3,
-    collapse_soma=True,
-    n_parallel=1,
-    point_radius=300,
-    root_id_from_point=False,
-    auth_token_key=None,
-    return_as="url",
-    verbose=True,
-):
-    if verbose:
-        t0 = time.time()
-
-    client = CAVEclient(
-        datastack, server_address=server_address, auth_token_key=auth_token_key
-    )
-
-    if root_id_from_point and root_id is None:
-        root_id = get_root_id_from_point(root_point, root_point_resolution, client)
-        if root_id == 0:
-            raise ValueError("Root point was not on any segmentation")
-
-    l2_sk, (l2dict, l2dict_r) = pcg_skel.pcg_skeleton(
-        root_id,
-        client=client,
-        refine="all",
-        root_point=root_point,
-        root_point_resolution=root_point_resolution,
-        collapse_soma=collapse_soma,
-        collapse_radius=10_000,
-        nan_rounds=20,
-        return_l2dict=True,
-        invalidation_d=invalidation_d,
-        root_point_search_radius=point_radius,
-        segmentation_fallback=False,
-        n_parallel=n_parallel,
-    )
-
-    if selection_point is not None:
-        l2_sk, _ = mask_skeleton(
-            root_id,
-            l2_sk,
-            l2dict,
-            selection_point=selection_point,
-            downstream=downstream,
-            client=client,
-            voxel_resolution=root_point_resolution,
-            radius=point_radius,
-        )
-
-    dfs, sbs = construct_cover_paths(
-        l2_sk,
-        n_choice,
-        spacing,
-        root_id,
-        segment_length_thresh,
-        client,
-        root_point_resolution,
-        interp_method,
-        selection_point,
-    )
-
-    csb = sb.ChainedStateBuilder(sbs)
-    if verbose:
-        print("\nComplete time: ", time.time() - t0)
-
-    return csb.render_state(
-        dfs, return_as=return_as, url_prefix=client.info.viewer_site()
-    )
 
 
 def generate_lvl2_proofreading(
